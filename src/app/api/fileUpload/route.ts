96b9a0c4fee3063f295f51a9ebb887f81458d83e
anish-singh-rawat <anishsinghrawat1>
2024-02-07 15:21:54 +0530

    add admin functionality in this repo like : CRUD , Auth, pagignation cookies set and so on

--- src/app/api/fileUpload/route.ts
+++ src/app/api/fileUpload/route.ts
@@ -1,9 +1,9 @@
 import {writeFile} from 'fs/promises'
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST (req){
+export async function POST (req : NextRequest){
     const data = await req.formData();
-    const file = data.get('file');
+    const file : any = data.get('file');
     if(file){
         const byteData = await file.arrayBuffer();
         const buffer = Buffer.from(byteData);
